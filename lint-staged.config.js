76f7118b9e10c0903caa225f3fe13060148dde0e
Readytowork co. ltd <readytowork@192.168.1.107>
2024-08-26 17:11:16 +0545

    chore: working on build issue

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,7 +2,7 @@ module.exports = {
   '{apps,shared}/**/*.{ts,tsx}': (files) => {
     return `nx affected --target=typecheck --files=${files.join(',')}`;
   },
-  '{apps,shared}/**/*.{js,ts,jsx,tsx,json,css}': [
+  '{apps,shared}/**/*.{js,ts,jsx,tsx,css}': [
     (files) => `nx affected:lint  --files=${files.join(',')}`,
     (files) => `nx format:write   --files=${files.join(',')}`
   ]
